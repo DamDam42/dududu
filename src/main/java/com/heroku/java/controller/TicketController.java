d0448c659bf44b63b12733056b446219f3737586
Ahmad Haziq <2022883316@student.uitm.edu.my>
2024-06-10 23:28:59 +0800

    a

---
+++ src/main/java/com/heroku/java/controller/TicketController.java
@@ -0,0 +1,52 @@
+package com.heroku.java.controller;
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.heroku.java.model.ticket;
+
+@Controller
+public class TicketController {
+    private final DataSource dataSource;
+
+    @Autowired
+    public TicketController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+   @PostMapping("/addTicket")
+    public String staffAddRoom(@ModelAttribute("addTicket")ticket ticket ){
+       
+
+        try {
+            Connection connection = dataSource.getConnection();
+            String sql = "INSERT INTO public.ticket(tickettype,ticketprice) VALUES(?,?)";
+            final var statement = connection.prepareStatement(sql);
+
+            String tickettype= ticket.getTicketType();
+            double ticketprice = ticket.getTicketPrice();
+            
+            
+            statement.setString(1, tickettype);
+            statement.setDouble(2, ticketprice);
+           
+            statement.executeUpdate();
+            
+            connection.close();
+                
+                } catch (Exception e) {
+                    e.printStackTrace();
+            
+                  
+                    return "redirect:/error";
+                }
+       
+                 
+            return "redirect:/index";
+         }
+}
---
+++ src/main/java/com/heroku/java/model/ticket.java
@@ -0,0 +1,35 @@
+package com.heroku.java.model;
+
+public class ticket {
+    private String tickettype;
+    private double ticketprice;
+    private int ticketid;
+
+    public ticket(){
+        
+    }
+
+    public String getTicketType() {
+        return this.tickettype;
+    }
+
+    public void setTicketType(String tickettype) {
+        this.tickettype = tickettype;
+    }
+
+    public double getTicketPrice() {
+        return this.ticketprice;
+    }
+
+    public void setTicketPrice(double ticketprice) {
+        this.ticketprice = ticketprice;
+    }
+
+    public int getTicketId() {
+        return this.ticketid;
+    }
+
+    public void setTicketType(int ticketid) {
+        this.ticketid = ticketid;
+    }
+}
